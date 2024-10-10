e4078dcb581138185acda18e8d7b50fa219eb0d7
Yehor Heiko <yehorheiko.developer@gmail.com>
2024-10-10 11:16:21 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/index.html",
+    "./src/pages"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
